4b47688b0dbeee4d7c7b2dd18b31e6ec97f14223
gstracquadanio <giovanni.stracquadanio@ed.ac.uk>
2022-01-18 13:48:03 +0000

    new: updated ci

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,16 +2,16 @@ import sys
 import subprocess as sh
 
 
-import subprocess
+# import subprocess
 
-try:
-    # initialise git
-    sh.run(["git", "init"], check=True)
+# try:
+#     # initialise git
+#     sh.run(["git", "init"], check=True)
 
-    # create repository
-    sh.run(["gh", "repo", "create","{{ cookiecutter.archetype_org}}/{{ cookiecutter.archetype_id }}", "-d", "{{ cookiecutter.archetype_description }}", "--private", "--confirm"], check=True)
+#     # create repository
+#     sh.run(["gh", "repo", "create","{{ cookiecutter.archetype_org}}/{{ cookiecutter.archetype_id }}", "-d", "{{ cookiecutter.archetype_description }}", "--private", "--confirm"], check=True)
 
-except subprocess.CalledProcessError as err:
-    print('Error:', err)
+# except subprocess.CalledProcessError as err:
+#     print('Error:', err)
 
 sys.exit(0)
--- {{cookiecutter.archetype_id}}/readme.md
+++ {{cookiecutter.archetype_id}}/readme.md
@@ -1,7 +1,8 @@
 # {{ cookiecutter.archetype_id }}
 
+![](https://img.shields.io/badge/current_version-v0.0.0-blue)
 ![](https://github.com/{{ cookiecutter.archetype_org }}/{{ cookiecutter.archetype_id }}/workflows/build/badge.svg)
-
+## Overview
 {{ cookiecutter.archetype_description }}
 
 ## Configuration
